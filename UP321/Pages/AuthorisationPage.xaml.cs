77c0e387d60503c1c3dc727fb23e5282f33d6226
212115 <s0n0f4ounk@gmail.com>
2023-11-15 13:19:40 +0300

    authorization system was improved

--- UP321/Pages/AuthorisationPage.xaml.cs
+++ UP321/Pages/AuthorisationPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UP321.Components;
 
 namespace UP321.Pages
 {
@@ -27,17 +28,19 @@ namespace UP321.Pages
 
         private void EntryButt_Click(object sender, RoutedEventArgs e)
         {
-            if (PasswordTb.Password == "111")
+            var studentList = App.db.Student.ToList();
+            var teacherList = App.db.Employee.ToList();
+            if (LoginTb.Text == "student" && !string.IsNullOrEmpty(studentList.Where(x => x.Id_Student == int.Parse(PasswordTb.Password)).ToString()))
             {
                 App.Role = "st";
                 NavigationService.Navigate(new NavigationPage());
             }
-            else if (PasswordTb.Password == "222")
+            else if (LoginTb.Text == "teacher" && !string.IsNullOrEmpty(teacherList.Where(x => x.Id_Employee == int.Parse(PasswordTb.Password)).ToString()))
             {
                 App.Role = "tch";
                 NavigationService.Navigate(new NavigationPage());
             }
-
+            else MessageBox.Show("Неправильный логин или пароль");
         }
     }
 }
